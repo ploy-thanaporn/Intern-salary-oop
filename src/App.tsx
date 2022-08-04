e433c4b26eaf354bbded2b8c1ae7a8a448582d4f
ploy-thanaporn <tnpsp1501@gmail.com>
2022-08-04 14:54:21 +0700

    complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { CEO, Junior, Manager, Senior } from "./Employee";
 
 function App() {
+  const ceo = new CEO("CEO", 25000, 3);
+  const junior = new Junior("Junior", 25000, 2);
+  const senior = new Senior("Senior", 25000, 3);
+  const manager = new Manager("Manager", 25000, 3);
+  // new Employee(new CEOSalary("g", 20000, "CEO"));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Salary Employee</h2>
+      <div className="table">
+        <table>
+          <thead>
+            <tr>
+              <th>Position</th>
+              <th>Salary</th>
+              <th>Bonus</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{junior.getPosition()}</td>
+              <td>{junior.getSalary().toLocaleString()} ฿</td>
+              <td>{junior.getBonus().toLocaleString()} ฿</td>
+            </tr>
+            <tr>
+              <td>{senior.getPosition()}</td>
+              <td>{senior.getSalary().toLocaleString()} ฿</td>
+              <td>{senior.getBonus().toLocaleString()} ฿</td>
+            </tr>
+            <tr>
+              <td>{manager.getPosition()}</td>
+              <td>{manager.getSalary().toLocaleString()} ฿</td>
+              <td>{manager.getBonus().toLocaleString()} ฿</td>
+            </tr>
+            <tr>
+              <td>{ceo.getPosition()}</td>
+              <td>{ceo.getSalary().toLocaleString()} ฿</td>
+              <td>{ceo.getBonus().toLocaleString()} ฿</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
---
+++ src/Employee.tsx
@@ -0,0 +1,121 @@
+export class Employee {
+  public position: "Junior" | "Senior" | "Manager" | "CEO";
+  protected salary: number;
+  protected bonus: number;
+
+  constructor(
+    salary: number,
+    bonus: number,
+    position: "Junior" | "Senior" | "Manager" | "CEO"
+  ) {
+    this.position = position;
+    this.salary = salary;
+    this.bonus = bonus;
+  }
+
+  //getter
+
+  getSalary() {
+    return this.salary;
+  }
+
+  getBonus() {
+    return this.bonus;
+  }
+}
+
+export class CEO extends Employee {
+  constructor(
+    position: "Junior" | "Senior" | "Manager" | "CEO",
+    salary: number,
+    bonus: number
+  ) {
+    super(salary, bonus, position);
+    this.salary = salary + 25000;
+    this.bonus = this.salary * bonus;
+  }
+
+  getPosition(): string {
+    return this.position;
+  }
+
+  getSalary() {
+    return this.salary;
+  }
+
+  getBonus() {
+    return this.bonus;
+  }
+}
+
+export class Junior extends Employee {
+  constructor(
+    position: "Junior" | "Senior" | "Manager" | "CEO",
+    salary: number,
+    bonus: number
+  ) {
+    super(salary, bonus, position);
+    this.salary = salary + 15000;
+    this.bonus = this.salary * bonus;
+  }
+
+  getPosition(): string {
+    return this.position;
+  }
+
+  getSalary() {
+    return this.salary;
+  }
+
+  getBonus() {
+    return this.bonus;
+  }
+}
+
+export class Senior extends Employee {
+  constructor(
+    position: "Junior" | "Senior" | "Manager" | "CEO",
+    salary: number,
+    bonus: number
+  ) {
+    super(salary, bonus, position);
+    this.salary = salary + 20000;
+    this.bonus = this.salary * bonus;
+  }
+
+  getPosition(): string {
+    return this.position;
+  }
+
+  getSalary() {
+    return this.salary;
+  }
+
+  getBonus() {
+    return this.bonus;
+  }
+}
+
+export class Manager extends Employee {
+  constructor(
+    position: "Junior" | "Senior" | "Manager" | "CEO",
+    salary: number,
+    bonus: number
+  ) {
+    super(salary, bonus, position);
+    this.salary = salary + 23000;
+    this.bonus = this.salary * bonus;
+  }
+
+  getPosition(): string {
+    return this.position;
+  }
+
+  getSalary() {
+    return this.salary;
+  }
+
+  getBonus() {
+    return this.bonus;
+  }
+}
