eed1d2186441930e2af81ef050a81bbdfd690df7
ploy-thanaporn <tnpsp1501@gmail.com>
2022-08-05 11:37:53 +0700

    feat: reverse string in js without array method

---
+++ src/Javascript.js
@@ -0,0 +1,14 @@
+// 1.How would you reverse a string in JavaScript from "dohtem yarra tpircsavaj gnisu tuohtiw tpircSavaJ ni gnirts a esrever uoy dluow woH" to "How would you reverse a string in JavaScript without using javascript array meth"
+// without using javascript array method?
+const str =
+  "dohtem yarra tpircsavaj gnisu tuohtiw tpircSavaJ ni gnirts a esrever uoy dluow woH";
+
+let newStr = " ";
+// str.length - 1 => last char , i > 0 => first char
+for (let i = str.length - 1; i > 0; i--) {
+  newStr += str[i];
+}
+console.log(newStr);
+
+// const newStr = str.split("");
+// console.log(newStr.reverse().join(""));
