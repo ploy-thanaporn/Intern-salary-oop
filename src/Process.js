cd1e8c566d525342d13329f118a786223186452b
ploy-thanaporn <tnpsp1501@gmail.com>
2022-08-11 15:23:57 +0700

    feat: promise

--- src/Process.js
+++ src/Process.js
@@ -22,3 +22,27 @@ function complete(result) {
 
 dowloading(url, complete);
 // result : is loading from 2Scoops.cnx/files.json -> Success download
+
+// ถ้ามีหลาย url ทำให้เรียก callback ซ้ำซ้อน (callback hell) แก้โดยใช้ Promise
+const connect = true;
+const url1 = "lauv.song/file1.json";
+
+function loading(url) {
+  console.log(`is loading from ${url}`);
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      if (connect) {
+        resolve(`load ${url} success`);
+      } else {
+        reject("error");
+      }
+    }, 3000);
+  });
+}
+loading(url1)
+  .then((output) => {
+    console.log(output);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
