7b5b0babd87e6ce0630070d9c36e41660d2d175f
ploy-thanaporn <tnpsp1501@gmail.com>
2022-08-11 14:26:38 +0700

    feat: asynchronous(sequence

--- src/Process.js
+++ src/Process.js
@@ -4,6 +4,21 @@ console.log("Begin");
 setTimeout(() => {
   console.log("is loading");
 }, 3000);
-
 console.log("End");
 // result: Begin -> End -> loading
+
+// จัดการทำงานแบบ Asynchronous ให้เป็นลำดับขั้นตอน
+const url = "2Scoops.cnx/files.json";
+function dowloading(url, callback) {
+  console.log(`is loading from ${url}`);
+  setTimeout(() => {
+    callback();
+  }, 4000);
+}
+
+function complete() {
+  console.log("Success download");
+}
+
+dowloading(url, complete);
+// result : is loading from 2Scoops.cnx/files.json -> Success download
