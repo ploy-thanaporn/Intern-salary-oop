206845bcafd556af3f011ac2e5ffc29c6f2c0bd8
ploy-thanaporn <tnpsp1501@gmail.com>
2022-08-15 14:41:17 +0700

    fix: update navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,21 +3,61 @@ import logo from "../logo.svg";
 
 function Nav() {
   return (
-    <nav className="sm:px-4 py-2.5 w-full z-20 top-0 left-0 border-b border-gray-200">
-      <div className="container flex justify-between flex-wrap items-center mx-auto">
-        <div className="border-r-[1.5px] border-[#D9D9D9]">
-          <a
-            href="https://20scoopscnx.com/"
-            className="flex items-center"
-            target="_blank"
-          >
-            <img src={logo} className="mr-3 h-14 sm:h-14" alt="logo" />
-          </a>
+    <nav className="px-4 py-2.5 w-full top-0 left-0 border-b border-gray-300 shadow-md">
+      <div className="container mx-auto flex justify-between">
+        <div>
+          <div className="flex flex-wrap items-center">
+            <div className="border-r-[1.5px] border-[#D9D9D9]">
+              <a
+                href="https://20scoopscnx.com/"
+                className="flex items-center"
+                target="_blank"
+              >
+                <img src={logo} className="mr-3 h-14 sm:h-14" alt="logo" />
+              </a>
+            </div>
+            <div className="hidden items-center md:flex md:w-auto">
+              <ul className="flex flex-col p-4 mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0">
+                <li>
+                  <Link
+                    to="/"
+                    className="block py-2 pr-4 pl-3 text-black hover:border-b-2 border-[#C03838] bg-transparent md:p-0"
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/salary"
+                    className="block py-2 pr-4 pl-3 text-black hover:border-b-2 border-[#C03838] bg-transparent md:p-0"
+                  >
+                    Salary
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="#"
+                    className="block py-2 pr-4 pl-3 text-black hover:border-b-2 border-[#C03838] bg-transparent md:p-0"
+                  >
+                    Services
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="#"
+                    className="block py-2 pr-4 pl-3 text-black hover:border-b-2 border-[#C03838] bg-transparent md:p-0"
+                  >
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-        <div className="flex md:order-2">
+        <div className="flex items-center">
           <button
             type="button"
-            className="text-white bg-[#DE4343] hover:bg-[#ff7e7e] focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0"
+            className="text-white bg-[#DE4343] hover:bg-[#ff7e7e] focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 "
           >
             Connect Wallet
           </button>
@@ -43,42 +83,6 @@ function Nav() {
               ></path>
             </svg>
           </button>
-        </div>
-        <div className="hidden items-center w-full md:flex md:w-auto">
-          <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
-            <li>
-              <Link
-                to="/"
-                className="block py-2 pr-4 pl-3 text-black hover:border-b-2 border-[#C03838] bg-transparent md:hover:border-b-2 md:p-0"
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/salary"
-                className="block py-2 pr-4 pl-3 text-black hover:border-b-2 border-[#C03838] bg-transparent md:hover:border-b-2 md:p-0"
-              >
-                Salary
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="#"
-                className="block py-2 pr-4 pl-3 text-black hover:border-b-2 border-[#C03838] bg-transparent md:hover:border-b-2 md:p-0"
-              >
-                Services
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="#"
-                className="block py-2 pr-4 pl-3 text-black hover:border-b-2 border-[#C03838] bg-transparent md:hover:border-b-2 md:p-0"
-              >
-                Contact
-              </Link>
-            </li>
-          </ul>
         </div>
       </div>
     </nav>
